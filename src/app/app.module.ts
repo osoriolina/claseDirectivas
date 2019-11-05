2c2547e4cb72d0f383b9f9cb11143ed7f4b2da02
Lina <osoriolina@gmail.com>
2019-11-05 10:28:58 +0100

    Proyecto inicial - falta ponerlo bonito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClientAreaComponent } from './client-area/client-area.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClientAreaComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
